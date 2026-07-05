bb20d8ce483110d13ee06405c25403d7fb5e4ad8
Zibo Zhang <zibo.zhang9@gmail.com>
2026-07-05 14:14:15 +0200

    Update ui_mainwindow.h

--- Teleoperation_ChronolabTT_Zibo/ui_mainwindow.h
+++ Teleoperation_ChronolabTT_Zibo/ui_mainwindow.h
@@ -1,9 +1,5 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
-**
-** Created by: Qt User Interface Compiler version 5.9.1
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
 #ifndef UI_MAINWINDOW_H
